194116d3a22ce0755ccc786b534e17b74a0362c4
Ryan-D-P <ryandp@mailfence.com>
2023-07-27 20:23:34 +1000

    Minor text and style changes.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
 export default function Home() {
   return (
     <main>
-      <h1>Experienced Frontend Developer</h1>
+      <h1>Experienced Frontend React Developer</h1>
       <div>
-        <p>I have working? experience in React development... in addition, I also possess a familiarity with WordPress development.</p>
+        <p>I have experience in React development, including: ...</p>
+        <p>In addition to my React development knowledge, I also possess a familiarity with WordPress development.</p>
       </div>
     </main>
   )
