079418106b7b4704b5ce4c5f63f8df4107f04ee3
Ryan-D-P <ryandp@mailfence.com>
2023-12-05 17:56:30 +1000

    Accordion menu and contents changes.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,41 +1,97 @@
 'use client'
 
 import { Box, Flex, Text } from "@chakra-ui/react";
+import { FaLinkedin, FaArrowDown } from "react-icons/fa";
 import TechSkills from "@/components/TechSkills";
+import Link from "next/link";
+import "./custom.css";
 
 export default function Home() {
   return (
-    <Box
-      as="main"
-      minH="100vh"
-      p="4"
-      backgroundImage="url('/portfolio-app/bg-white.jpg')"
-      backgroundSize="cover"
-      backgroundRepeat="no-repeat"
-    >
-      <Box px={10}>
-        <Text as="h1" fontSize={{ base: "1.5em", sm: "3em", md: "3.5em" }} fontWeight={800}>Experienced Web Developer</Text>
-        <Text fontWeight={800} color="customBlue.500">Ryan Pittock</Text>
-        <Box>
-          <Text my={3} maxW="800px" fontWeight={500}>I am an experienced web developer and programmer with a background in React and WordPress.</Text>
-          <Text my={3} maxW="800px" fontWeight={500}>I have worked in a web development company on the Gold Coast as part of their development team for over a year. Working with this company is what introduced me to WordPress development since they use it for their projects; I can prove to be highly adaptable and able to learn new concepts and technologies quickly, while I love to continually grow my skillset in this industry and increase the services I can provide to clients and businesses.</Text>
+    <>
+      <Box as="section" minH="100vh" p={10} pos="relative" backgroundImage="url('/portfolio-app/bg-white.jpg')" backgroundSize="cover" backgroundRepeat="no-repeat">
+        <Box px={10}>
+          <Box as="header">
+            <Text as="h1" fontSize={{ base: "1.5em", sm: "3em", md: "3.5em" }} fontWeight={800}>Experienced Web Developer</Text>
+            <Text fontWeight={800} color="customBlue.500">Ryan Pittock</Text>
+          </Box>
+          <Box as="main">
+            <Text my={5} maxW="800px" fontWeight={500}>I am an experienced web developer and programmer with a background in React and WordPress.</Text>
+            <Text my={5} maxW="800px" fontWeight={500}>I have industry experience working in a web development company on the Gold Coast as part of their development team. Working with this company is what first introduced me to WordPress development since they use it for their projects.</Text>
+            <Text my={5} maxW="800px" fontWeight={500}>Due to my particular background in web development: the services I provide to clients are mostly related to headless React applications and WordPress sites; however I can even do backend server-side web programming as well, such as Node.js Express and Python Django to give examples.</Text>
+            <Text my={5} maxW="800px" fontWeight={500}>I enjoy what I do and prove to be highly adaptable and able to learn new concepts and technologies quickly, while I love to continually grow my skill set in this industry and increase the services I can provide to clients and businesses.</Text>
+          </Box>
+          <Flex gap={5} mb={5}>
+            <a href="https://www.linkedin.com/in/ryan-pittock-61169229b/" target="_blank">
+              <FaLinkedin color="#0a66c2" size="2em" />
+            </a>
+          </Flex>
+          <Flex
+            flexDirection="column"
+            alignItems="center"
+            gap={3}
+            pos={{ xl: "absolute" }}
+            bottom={{ xl: "100px" }}
+            left={{ xl: "50%" }}
+            transform={{ xl: "translateX(-50%)" }}
+          >
+            <Text fontWeight={800} color="customBlue.500">See below for services I provide</Text>
+            <Link href="#skills">
+              <Box
+                bg="customBlue.500"
+                p={2}
+                borderRadius="50%"
+                transitionDuration="0.5s"
+                _hover={{
+                  transform: "scale(1.2)",
+                }}
+              >
+                <FaArrowDown size="2em" color="#ffffff" />
+              </Box>
+            </Link>
+          </Flex>
         </Box>
-        <Box mt={100} mb={10}>
-          <Text fontWeight={800} color="customBlue.500">Services I Provide</Text>
-        </Box>
-        <Box px={{ base: 0, xl: 250 }}>
+      </Box>
+      <Box as="section" minH="100vh" p={10}>
+        <Box px={{ base: 0, xl: 100 }}>
           <Flex flexDirection={{ base: "column", xl: "row" }} justifyContent="space-around" gap={5}>
             <TechSkills 
-              tech="react" 
-              skills="Create custom functional components and hooks. Develop headless-CMS applications with WordPress. Make API calls using query libraries such as React Query. Implement custom HTML and CSS layouts and styling such as flexbox, grids, etc. as well as frameworks such as Bootstrap, Tailwind CSS, and Chakra UI. Develop with the Next.js framework and deploy on a Vercel server. Test applications with Cypress end-to-end testing and by creating unit tests."
+              tech="react"
+              skillsTitles={[
+                "Create custom functional components and hooks",
+                "Develop headless-CMS applications with WordPress",
+                "Make API calls using query libraries such as React Query",
+                "Develop with Next.js and deploy on Vercel",
+                "Test applications with Cypress",
+              ]}
+              skillsDescriptions={[
+                "Implement any design and functionality into your app with custom components using the most up-to-date React code. Included in this is custom HTML and CSS layouts and styling such as flexbox, grids, as well as popular frontend frameworks such as Bootstrap, Tailwind CSS, and Chakra UI.",
+                "Combine the power of React and WordPress by creating any kind of React app which can also use WordPress as its content-management system and much more. With headless apps, you can easily create both an app and a standard WordPress site that share the same backend.",
+                "Efficiently pull any kind of API data into your app using the most updated best practices with React fetching libraries such as React Query; this allows your app to add features such as API data caching and more.",
+                "Develop your app with the most popular modern React framework: Next.js. Next is the top recommended way to create a React app according to the offical React docs. The company behind Next.js (Vercel) also provides modern web hosting for your application.",
+                "Applications are fully end-to-end tested using Cypress. I can create tests to gurantee your app is production ready and all of the features function as required.",
+              ]}
             />
             <TechSkills 
-              tech="wp" 
-              skills="Develop numerous custom plugins and themes using PHP code. Effectively modify existing plugins/themes via hooks and filters. Create a WooCommerce ecommerce site for your business. Easily implement any web design into your site with Elementor page builder. Create custom post types and meta fields to define any kind of complex data for your website or application. Host and maintain your WordPress website including keeping plugins/themes and WordPress core up-to-date. Allow you to change your website's content at any time through the site dashboard."
+              tech="wp"
+              skillsTitles={[
+                "Develop custom plugins and themes",
+                "Create an ecommerce site with WooCommerce",
+                "Implement any web design into your site with Elementor",
+                "Host and maintain your WordPress website",
+                "Allow you to change your website's content",
+              ]}
+              skillsDescriptions={[
+                "My knowledge of PHP code allows me to add any custom code to your WordPress site: including any custom plugins and themes which you require; I can also effectively modify existing site plugins and themes to fit your needs.",
+                "Boost sales with your own e-commerce site. I have industry experience with WooCommerce, the most popular e-commerce platform for WordPress.",
+                "Want a beautiful WordPress site? Page-builders such as Elementor and WPBakery make creating well-designed pages effortless.",
+                "Easily host and maintain your site with Hostinger: a modern WordPress hosting platform. I can also ensure all of your site's plugins, themes, and WordPress core are up-to-date.",
+                "Change any of your site's content at any time using the WordPress site dashboard. WordPress is highly customisable, and does not require any coding knowledge for simple content changes such as text, theme styles, and images.",
+              ]}
             />
           </Flex>
         </Box>
       </Box>
-    </Box>
+    </>
   )
 }
--- src/components/TechSkills.tsx
+++ src/components/TechSkills.tsx
@@ -1,21 +1,40 @@
 'use client'
 
-import { Box, Text, Flex, Image } from "@chakra-ui/react";
+import { Box, Text, Image, Flex, Accordion, AccordionItem, AccordionButton, AccordionPanel, AccordionIcon, } from "@chakra-ui/react";
 
 interface TechSkillsProps {
     tech: string;
-    skills: string;
+    skillsTitles: Array<string>;
+    skillsDescriptions: Array<string>;
 }
 
-const TechSkills = ({ tech, skills }: TechSkillsProps) => {
+const TechSkills = ({ tech, skillsTitles, skillsDescriptions }: TechSkillsProps) => {
   return (
-    <Flex flexDirection="column" alignItems="center">
-        <Box mb={5}>
-            <Image src={`/portfolio-app/${tech}_logo.svg`} alt={`/portfolio-app/${tech}_logo.svg`} w={100} h={100} />
-        </Box>
-        <Box minW={280}>
-            <Text maxW="400px" fontWeight={500}>{skills}</Text>
-        </Box>
+    <Flex flexDirection="column" flex="1" id="skills">
+      <Box mb={5} alignSelf="center">
+          <Image src={`/portfolio-app/${tech}_logo.svg`} alt={`/portfolio-app/${tech}_logo.svg`} w={100} h={100} />
+      </Box>
+      <Accordion allowMultiple>
+          {
+            skillsTitles.map((title, index) => (
+              <AccordionItem key={index}>
+                <h2>
+                  <AccordionButton p={4} _expanded={{ bg: "customBlue.500", color: "white" }}>
+                      <Box as="span" flex="1" textAlign="left">
+                        <Text>
+                          {title}
+                        </Text>
+                      </Box>
+                      <AccordionIcon />
+                  </AccordionButton>
+                </h2>
+                <AccordionPanel bg="customBlue.100" p={4}>
+                  {skillsDescriptions[index]}
+                </AccordionPanel>
+            </AccordionItem>
+            ))
+          }
+      </Accordion>
     </Flex>
   );
 };
