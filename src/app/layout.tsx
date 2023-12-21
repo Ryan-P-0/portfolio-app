30c25c293d332ce199d9abc8616f4e349703da6f
Ryan-D-P <ryandp@mailfence.com>
2023-12-22 07:32:26 +1000

    Meta keywords.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,9 +7,29 @@ const montserrat = Montserrat({
 });
 
 export const metadata = {
-  title: 'Experienced Web Developer',
-  description: 'I am an experienced web developer and programmer with a background in React and WordPress.',
-}
+  title: "Experienced Web Developer",
+  description: "I am an experienced web developer and programmer with a background in React and WordPress.",
+  keywords: [
+    "web developer brisbane",
+    "freelance web developer brisbane",
+    "experienced web developer brisbane",
+    "brisbane web developer",
+    "brisbane freelance web developer",
+    "brisbane experienced web developer",
+    "react developer brisbane",
+    "freelance react developer brisbane",
+    "experienced react developer brisbane",
+    "brisbane react developer",
+    "brisbane freelance react developer",
+    "brisbane experienced react developer",
+    "wordpress developer brisbane",
+    "freelance wordpress developer brisbane",
+    "experienced wordpress developer brisbane",
+    "brisbane wordpress developer",
+    "brisbane freelance wordpress developer",
+    "brisbane experienced wordpress developer",
+  ],
+};
 
 export default function RootLayout({
   children,
